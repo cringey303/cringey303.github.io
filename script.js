39667af2378f0c00e00b8b4f384ce9c555fafa83
cringey303 <lucas@the-b.org>
2025-12-26 19:44:41 -0800

    added confetti script added accent on résume

--- script.js
+++ script.js
@@ -27,3 +27,27 @@ function updateSupremeClock() {
 // Update every second
 setInterval(updateSupremeClock, 1000);
 updateSupremeClock(); // Run immediately on load
+
+// -- Logo Confetti --
+    const logo = document.getElementById('confetti');
+
+    if (logo) {
+        logo.addEventListener('click', (e) => {
+
+            // get position of the text for confetti origin
+            const rect = logo.getBoundingClientRect();
+            // convert pixels to 0-1 range required by the library
+            const x = (rect.left + rect.width / 2) / window.innerWidth;
+            const y = (rect.top + rect.height / 2) / window.innerHeight;
+
+            if (typeof confetti === 'function') {
+                confetti({
+                    particleCount: 99,
+                    spread: 999,
+                    origin: { x: x, y: y },
+                    colors: ['rebeccapurple'],
+                    disableForReducedMotion: true
+                });
+            }
+        });
+    };
